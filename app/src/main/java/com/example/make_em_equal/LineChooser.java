330d6fd72827c6cced5cb62ffbad786d17e78eb0
RachelRebecca <rachelrebecca67@gmail.com>
2021-08-02 00:38:34 -0400

    drastically updated content main, made game playable (layout doesn't make sense yet though)

---
+++ app/src/main/java/com/example/make_em_equal/LineChooser.java
@@ -0,0 +1,45 @@
+package com.example.make_em_equal;
+
+import java.util.Random;
+
+public class LineChooser {
+
+    Integer[] num1aList;
+    Integer[] num1bList;
+    Integer[] num2aList;
+    Integer[] num2bList;
+    Double[] answerList;
+    int lineChoice;
+
+    public LineChooser() {
+        num1aList = new Integer[]{1, 3, 7, 2, 1, 2, 1, 7, 9, 5, 8, 5, 8, 8, 6, 6, 9, 7, 5, 2};
+        num1bList = new Integer[]{2, 4, 1, 1, 2, 3, 6, 0, 2, 5, 3, 3, 2, 3, 2, 2, 5, 2, 4, 1};
+        num2aList = new Integer[]{4, 4, 1, 4, 8, 4, 8, 0, 3, 4, 2, 6, 6, 4, 1, 3, 1, 8, 8, 3};
+        num2bList = new Integer[]{2, 3, 6, 3, 9, 6, 2, 7, 4, 4, 4, 9, 4, 6, 3, 1, 4, 9, 1, 7};
+        answerList = new Double[]{2.0, 12.0, 7.0, 1.0, -1.0, .6666, 16.0, 7.0, 7.0, 1.0, 24.0,
+                15.0, 10.0, 24.0, 3.0, 3.0, 4.0, 72.0, 9.0, 21.0};
+
+        Random rand = new Random();
+        lineChoice = rand.nextInt(num1aList.length);
+    }
+
+    public int getLineChoice(){
+        return this.lineChoice;
+    }
+
+    public int getNum1A(){
+        return num1aList[getLineChoice()];
+    }
+    public int getNum1B(){
+        return num1bList[getLineChoice()];
+    }
+    public int getNum2A(){
+        return num2aList[getLineChoice()];
+    }
+    public int getNum2B(){
+        return num2bList[getLineChoice()];
+    }
+    public double getAnswer(){
+        return answerList[getLineChoice()];
+    }
+}
--- app/src/main/java/com/example/make_em_equal/MainActivity.java
+++ app/src/main/java/com/example/make_em_equal/MainActivity.java
@@ -12,10 +12,23 @@ import com.example.make_em_equal.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private LineChooser lineChooser;
+    private TextView contentMessage;
+    private TextView num1a;
+    private TextView space1;
+    private TextView num1b;
+    private TextView num2a;
+    private TextView space2;
+    private TextView num2b;
+    private TextView answer1;
+    private TextView answer2;
+    private TextView answerHint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +36,38 @@ public class MainActivity extends AppCompatActivity {
         setContent();
         setSupportActionBar(binding.toolbar);
         setupFAB();
+
+        contentMessage = binding.includeContentMain.contentMessage;
+        num1a = binding.includeContentMain.num1a;
+        space1 = binding.includeContentMain.space1;
+        num1b = binding.includeContentMain.num1b;
+        num2a = binding.includeContentMain.num2a;
+        space2 = binding.includeContentMain.space2;
+        num2b = binding.includeContentMain.num2b;
+        answer1 = binding.includeContentMain.answer1;
+        answer2 = binding.includeContentMain.answer2;
+        //answer = binding.includeContentMain.answer;
+
+        startNewGame();
+    }
+
+    private void startNewGame() {
+        lineChooser = new LineChooser();
+        String num1aString = String.valueOf(lineChooser.getNum1A());
+        num1a.setText(num1aString);
+        String num1bString = String.valueOf(lineChooser.getNum1B());
+        num1b.setText(num1bString);
+        String num2aString = String.valueOf(lineChooser.getNum2A());
+        num2a.setText(num2aString);
+        String num2bString = String.valueOf(lineChooser.getNum2B());
+        num2b.setText(num2bString);
+        String answer1String = String.valueOf(lineChooser.getNum1A() + " " + lineChooser.getNum1B());
+        answer1.setText(answer1String);
+        //getNum2a has the 0 for 07 if you want to remove do it in an if statement.
+        String answer2String = String.valueOf(lineChooser.getNum2A() + " " + lineChooser.getNum2B());
+        answer2.setText(answer2String);
+        //String hintString = String.valueOf(lineChooser.getAnswer());
+        //answer.setText(hintString);
     }
 
     private void setContent()
@@ -64,4 +109,65 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void selectOperator1(View view) {
+        space1.setText(((Button) view).getText().toString());
+        switch (((Button) view).getText().toString()){
+            case "+":
+                answer1.setText(String.valueOf(lineChooser.getNum1A() + lineChooser.getNum1B()));
+                break;
+            case "-":
+                answer1.setText(String.valueOf(lineChooser.getNum1A() - lineChooser.getNum1B()));
+                break;
+            case "x": case "X":
+                answer1.setText(String.valueOf(lineChooser.getNum1A() * lineChooser.getNum1B()));
+                break;
+            case "/":
+                answer1.setText(String.valueOf(lineChooser.getNum1A() / lineChooser.getNum1B()));
+                break;
+            case "_": case " ": case "reset":
+                space1.setText(R.string.space);
+                String answer1Text = num1a.getText().toString() + " " + num1b.getText().toString();
+                answer1.setText(answer1Text);
+                break;
+        }
+
+        if (isEqual()){
+            contentMessage.setText(R.string.great_job);
+        }
+    }
+    public void selectOperator2(View view) {
+        space2.setText(((Button) view).getText().toString());
+        switch (((Button) view).getText().toString()) {
+            case "+":
+                answer2.setText(String.valueOf(lineChooser.getNum2A() + lineChooser.getNum2B()));
+                break;
+            case "-":
+                answer2.setText(String.valueOf(lineChooser.getNum2A() - lineChooser.getNum2B()));
+                break;
+            case "x":
+            case "X":
+                answer2.setText(String.valueOf(lineChooser.getNum2A() * lineChooser.getNum2B()));
+                break;
+            case "/":
+                answer2.setText(String.valueOf(lineChooser.getNum2A() / lineChooser.getNum2B()));
+                break;
+            case "_": case " ": case "reset":
+                space1.setText(R.string.space);
+                String answer1Text = num1a.getText().toString() + " " + num1b.getText().toString();
+                answer1.setText(answer1Text);
+                break;
+
+        }
+       if (isEqual()){
+           contentMessage.setText(R.string.great_job);
+       }
+    }
+
+    public boolean isEqual(){
+        boolean isEqual = false;
+        if (answer1.getText().toString().equals(answer2.getText().toString())){
+            isEqual = true;
+        }
+        return isEqual;
+    }
 }
